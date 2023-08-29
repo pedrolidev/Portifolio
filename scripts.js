883c0c3821317673c9bcb0d3dbb822597f7a42c5
Pedro Oliveira <126485301+ppedr0liveira@users.noreply.github.com>
2023-08-29 09:57:52 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 200) {
+    header.style.background = '#191919'
+    } else {
+    header.style.background = 'transparent'
+    }
+
+})
